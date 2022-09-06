9edcbabdca5703632b97e6dfc2a4e30a99a7d837
thianparallel <thian.lopez@parallelpublicworks.com>
2022-09-06 12:35:19 -0500

    feat: Option for returning the response like is comming from the request
    Option for returning the response like is comming from the request

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -141,7 +141,9 @@ var DrupalOAuth = /*#__PURE__*/function () {
 
       var _extraParams = extraParams,
           _extraParams$useDefau = _extraParams.useDefaultJsonApiPath,
-          useDefaultJsonApiPath = _extraParams$useDefau === void 0 ? true : _extraParams$useDefau;
+          useDefaultJsonApiPath = _extraParams$useDefau === void 0 ? true : _extraParams$useDefau,
+          _extraParams$returnRe = _extraParams.returnResponse,
+          returnResponse = _extraParams$returnRe === void 0 ? false : _extraParams$returnRe;
 
       if (body && !(body instanceof File)) {
         body = JSON.stringify(body);
@@ -165,7 +167,7 @@ var DrupalOAuth = /*#__PURE__*/function () {
       }
 
       return Promise.resolve(fetch(url, init)).then(function (resp) {
-        return Promise.resolve(_this2.verifyResponse(resp)).then(function (validResponse) {
+        return returnResponse ? resp : Promise.resolve(_this2.verifyResponse(resp)).then(function (validResponse) {
           var _exit3 = false;
 
           var _temp3 = function () {
